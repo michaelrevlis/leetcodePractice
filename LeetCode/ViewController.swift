b926cde139f5f6044a8627f89705233352e59f38
MIR <michaelrevlis@gmail.com>
2019-04-05 18:09:48 +0800

    Initial Commit

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by MichaelRevlis on 2019/4/5.
+//  Copyright © 2019年 MichaelRevlis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
